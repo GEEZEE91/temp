46930c9a8afb60be964c776a46c4ba1dacc5ee25
rocky <rochak.ms@gmail.com>
2023-02-06 01:33:59 +0800

    ADDED fkught and hotel style

--- travel-planner-2a0063fe1b3203f7048415f20e708bf3077e5d9c/server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Trip = require('./Trip');
-const Flight = require('./Flight');
-const Hotel = require('./Hotel');
-
-module.exports = { User , Trip, Flight, Hotel};
