a5acf52a3f6250913f62b13ee6cf5c6e6f04796d
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2023-02-06 00:14:55 +1000

    Update index.js

--- travel-planner-2a0063fe1b3203f7048415f20e708bf3077e5d9c/client/src/components/Landing/index.js
+++ travel-planner-2a0063fe1b3203f7048415f20e708bf3077e5d9c/client/src/components/Landing/index.js
@@ -2,37 +2,34 @@ import React from "react";
 import Login from "./Login";
 import Signup from "./Signup";
 import "./landing.css";
+import { Link } from 'react-scroll';
+import Video from '../../assets/coastline.mp4';
+
 
 const Landing = () => {
   return (
     <>
-      <div className="container">
-        <div className={"parallax text-center1"}>
-          <img
-            draggable="false"
-            id="mountain"
-            src="https://www.design-fenix.com.ar/codepen/parallax/mountain.png"
-            alt=""
-          />
+      <div className="hero w-full h-half relative text-white">
+				<video
+					autoPlay
+					loop
+					muted
+					id="video"
+					className="w-full object-cover z-[-5]"
+				>
+					<source src={Video} type="video/mp4" />
+				</video>
 
-          <img
-            draggable="false"
-            id="island"
-            src="https://www.design-fenix.com.ar/codepen/parallax/island.png"
-            alt=""
-          />
-          <img
-            draggable="false"
-            id="boat"
-            src="https://www.design-fenix.com.ar/codepen/parallax/boat.png"
-            alt=""
-          />
+				<div className="overlay bg-[#00000016]"></div>
+
+			
+					
+				
+			</div>
           <h1 className="text-center1 land-header">TRAVELLER MATE</h1>
           <div className="lowBtn">
             <Login /> <strong>- OR -</strong> <Signup />
           </div>
-        </div>
-      </div>
     </>
   );
 };
